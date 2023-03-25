c338d041ffa6fce53db2a585efb8fd49d8e22632
SNatt <114608951+SNatt@users.noreply.github.com>
2023-03-25 16:14:51 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,3 @@
 base_url = 'https://b2c.passport.rt.ru/'
 valid_email = 'rtkmtesting@yandex.ru'
 valid_password = 'Qwertyui198'
-
-
-
-incor_firstname_en = 'Ivan'
-incor_firstname_ru = 'Иван32'
-incor_firstname_31 = 'Иванрнглрвапкнеритоалеьмгоекртш'
